f00423a7f32a32ce51cb71b72d256da1b89df3c7
Robson Schneider da Natividade <rsnatividade@gmail.com>
2016-12-28 14:01:19 -0200

    Update script.sql

--- script/script.sql
+++ script/script.sql
@@ -1,40 +1,144 @@
+CREATE TABLE `adm_agenda` (
+  `id_agenda` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_agenda`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `adm_veiculo` (
+  `id_veiculo` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_descricao` varchar(255) DEFAULT NULL,
+  `id_modelo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_veiculo`),
+  KEY `FKqdqgeg5nrtnwhbhh3a0vc31tv` (`id_modelo`),
+  CONSTRAINT `FKqdqgeg5nrtnwhbhh3a0vc31tv` FOREIGN KEY (`id_modelo`) REFERENCES `adm_modelo` (`id_modelo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `adm_modelo` (
+  `id_modelo` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_nome` varchar(255) DEFAULT NULL,
+  `id_marca` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_modelo`),
+  KEY `FKch9gmrh86cs5awui26vafgoua` (`id_marca`),
+  CONSTRAINT `FKch9gmrh86cs5awui26vafgoua` FOREIGN KEY (`id_marca`) REFERENCES `adm_marca` (`id_marca`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `adm_marca` (
+  `id_marca` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_marca`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `adm_disponibilidade` (
+  `id_disponibilidade` int(11) NOT NULL AUTO_INCREMENT,
+  `dt_disponibilidade` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_disponibilidade`),
+  CONSTRAINT `FKa87jda8e6rf97sa2rednr7i0x` FOREIGN KEY (`id_disponibilidade`) REFERENCES `adm_agenda` (`id_agenda`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `adm_agendamento` (
+  `id_agendamento` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_agendamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `adm_orcamento` (
+  `id_orcamento` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_orcamento`),
+  CONSTRAINT `FKkwt34djuteruce0xym1tsl0v9` FOREIGN KEY (`id_orcamento`) REFERENCES `adm_veiculo` (`id_veiculo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `ca_autorizacao` (
   `id_autorizacao` int(11) NOT NULL AUTO_INCREMENT,
-  `tx_nome_autorizacao` varchar(45) NOT NULL,
-  `tx_codigo_autorizacao` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_autorizacao`),
-  UNIQUE KEY `tx_codigo_autorizacao_UNIQUE` (`tx_codigo_autorizacao`)
+  `tx_codigo_autorizacao` varchar(255) DEFAULT NULL,
+  `tx_nome_autorizacao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_autorizacao`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `ca_perfil` (
   `id_perfil` int(11) NOT NULL AUTO_INCREMENT,
-  `tx_nome_perfil` varchar(45) NOT NULL,
+  `tx_nome_perfil` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_perfil`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `ca_perfil_autorizacao` (
+  `Perfil_id_perfil` int(11) NOT NULL,
+  `autorizacoes_id_autorizacao` int(11) NOT NULL,
+  KEY `FKpt8gd3lcy65tcuadoqwj7tenp` (`autorizacoes_id_autorizacao`),
+  KEY `FK3xjmfb9etmwhrvo4gawlc2aj` (`Perfil_id_perfil`),
+  CONSTRAINT `FK3xjmfb9etmwhrvo4gawlc2aj` FOREIGN KEY (`Perfil_id_perfil`) REFERENCES `ca_perfil` (`id_perfil`),
+  CONSTRAINT `FKpt8gd3lcy65tcuadoqwj7tenp` FOREIGN KEY (`autorizacoes_id_autorizacao`) REFERENCES `ca_autorizacao` (`id_autorizacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `ca_perfil_usuario` (
+  `Usuario_id_usuario` int(11) NOT NULL,
+  `perfis_id_perfil` int(11) NOT NULL,
+  KEY `FKp2ssg5fp5vbtl2hyxaujgn5rm` (`perfis_id_perfil`),
+  KEY `FKlat3fh7eesq3rveesrups4h7j` (`Usuario_id_usuario`),
+  CONSTRAINT `FKlat3fh7eesq3rveesrups4h7j` FOREIGN KEY (`Usuario_id_usuario`) REFERENCES `ca_usuario` (`id_usuario`),
+  CONSTRAINT `FKp2ssg5fp5vbtl2hyxaujgn5rm` FOREIGN KEY (`perfis_id_perfil`) REFERENCES `ca_perfil` (`id_perfil`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `ca_usuario` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `tx_login` varchar(45) NOT NULL,
-  `tx_nome_usuario` varchar(45) NOT NULL,
-  `tx_senha` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE KEY `tx_login_UNIQUE` (`tx_login`)
+  `tx_login` varchar(255) DEFAULT NULL,
+  `tx_nome_usuario` varchar(255) DEFAULT NULL,
+  `tx_senha` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `ca_perfil_autorizacao` (
-  `id_perfil` int(11) DEFAULT NULL,
-  `id_autorizacao` int(11) DEFAULT NULL,
-  KEY `fk_id_perfil_idx` (`id_perfil`),
-  KEY `fk_id_autorizacao_idx` (`id_autorizacao`),
-  CONSTRAINT `fk_pa_id_autorizacao` FOREIGN KEY (`id_autorizacao`) REFERENCES `ca_autorizacao` (`id_autorizacao`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pa_id_perfil` FOREIGN KEY (`id_perfil`) REFERENCES `ca_perfil` (`id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `ger_cargo` (
+  `id_cargo` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_cargo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `ca_perfil_usuario` (
-  `id_perfil` int(11) DEFAULT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  KEY `fk_id_perfil_idx` (`id_perfil`),
-  KEY `fk_id_usuario_idx` (`id_usuario`),
-  CONSTRAINT `fk_pu_id_perfil` FOREIGN KEY (`id_perfil`) REFERENCES `ca_perfil` (`id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pu_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `ca_usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `ger_endereco` (
+  `id_endereco` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_complemento` varchar(255) DEFAULT NULL,
+  `tx_logradouro` varchar(255) DEFAULT NULL,
+  `vl_numero` int(11) DEFAULT NULL,
+  `tx_tipo_contato` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_endereco`),
+  CONSTRAINT `FKra7p7elqbapfpygfvcpu69kkj` FOREIGN KEY (`id_endereco`) REFERENCES `ger_pessoa` (`id_pessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `ger_papel` (
+  `DTYPE` varchar(31) NOT NULL,
+  `id_papel` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_descricao` varchar(255) DEFAULT NULL,
+  `id_cargo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_papel`),
+  KEY `FKa87mp7e7khd4amwklg8t4lg86` (`id_cargo`),
+  CONSTRAINT `FKa87mp7e7khd4amwklg8t4lg86` FOREIGN KEY (`id_cargo`) REFERENCES `ger_cargo` (`id_cargo`),
+  CONSTRAINT `FKftth8ayc13kqam4kqg8u0xmjc` FOREIGN KEY (`id_papel`) REFERENCES `ger_pessoa` (`id_pessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `ger_pessoa` (
+  `DTYPE` varchar(31) NOT NULL,
+  `id_pessoa` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_nome` varchar(255) DEFAULT NULL,
+  `tx_cpf` varchar(255) DEFAULT NULL,
+  `tx_cnpj` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_pessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `ger_telefone` (
+  `id_telefone` int(11) NOT NULL AUTO_INCREMENT,
+  `tx_descricao` varchar(255) DEFAULT NULL,
+  `tx_tipo_contato` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_telefone`),
+  CONSTRAINT `FKijoxr3kqs4ii3n2fm9rt4b3ye` FOREIGN KEY (`id_telefone`) REFERENCES `ger_pessoa` (`id_pessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `op_categoria` (
+  `DTYPE` varchar(31) NOT NULL,
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `op_produto` (
+  `DTYPE` varchar(31) NOT NULL,
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_produto`),
+  CONSTRAINT `FKr13bab0pgwmgy3yoiqxy645l4` FOREIGN KEY (`id_produto`) REFERENCES `adm_orcamento` (`id_orcamento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
